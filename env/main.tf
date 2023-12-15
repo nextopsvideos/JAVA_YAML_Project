23d276413daa3f30354cdaf3401fc6eb2f2f94a4
nextopsvideos <nextopsvideos@gmail.com>
2023-12-15 07:04:02 +0530

    terraform files added and pom.xml updated with mysql as default

---
+++ env/main.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.80.0"
+    }
+  }
+  backend "azurerm" {    
+  } 
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "example" {
+  for_each = toset(var.environments)
+
+  name     = "DevOpsRG"
+  location = var.location
+}
+
+# App Service Plan
+resource "azurerm_service_plan" "example" {
+  for_each = toset(var.environments)
+
+  name                = "nextopsasp-${each.key}-01"
+  resource_group_name = azurerm_resource_group.example[each.key].name
+  location            = azurerm_resource_group.example[each.key].location
+  os_type             = "Linux"
+  sku_name            = "S1"
+}
+
+# Linux Web App
+resource "azurerm_linux_web_app" "example" {
+  for_each = toset(var.environments)
+
+  name                = "nextopsapp-${each.key}-01"
+  resource_group_name = azurerm_resource_group.example[each.key].name
+  location            = azurerm_resource_group.example[each.key].location
+  service_plan_id     = azurerm_service_plan.example[each.key].id
+  site_config {
+    # Additional site configurations go here
+  }
+
+  app_settings = {
+    "WEBSITE_JAVA_CONTAINER"          = "TOMCAT"
+    "WEBSITE_JAVA_CONTAINER_VERSION"  = "8.5"
+  }
+}
+
+# MySQL Server
+resource "azurerm_mysql_server" "example" {
+  for_each = toset(var.environments)
+
+  name                = "nextopsmysql-${each.key}01"
+  resource_group_name = azurerm_resource_group.example[each.key].name
+  location            = azurerm_resource_group.example[each.key].location
+  sku_name            = "GP_Gen5_2"
+  version             = "5.7"
+  ssl_enforcement_enabled = true
+  administrator_login          = "petclinic"
+  administrator_login_password = "Admin@12345678"
+}
+
+# MySQL Firewall Rule
+resource "azurerm_mysql_firewall_rule" "example" {
+  for_each = toset(var.environments)
+
+  name                = "nextopsmysqlrule-${each.key}"
+  resource_group_name = azurerm_resource_group.example[each.key].name
+  server_name         = azurerm_mysql_server.example[each.key].name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
+}
+
+# MySQL Database
+resource "azurerm_mysql_database" "example" {
+  for_each = toset(var.environments)
+
+  name                = "petclinic"
+  resource_group_name = azurerm_resource_group.example[each.key].name
+  server_name         = azurerm_mysql_server.example[each.key].name
+  charset             = "utf8mb4"
+  collation           = "utf8mb4_unicode_ci"
+}
---
+++ env/variables.tf
@@ -0,0 +1,11 @@
+# Variables
+variable "environments" {
+  description = "List of environments"
+  type        = list(string)
+  default     = ["dev", "staging"]
+}
+
+variable "location" {
+  description = "The location for all resources"
+  default     = "East US"
+}
